9646d6b83d4e2b84016e6655caee1917bad0753a
Deepesh Sharma <sharmadeepesh2002@gmail.com>
2020-03-16 13:53:15 +0530

    Improved Desktop View

--- recommend/urls.py
+++ recommend/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('artist/', views.artist_detail, name="artist"),
     path('artist/<str:artist_name>/', views.artist_detail, name="artist"),
-    path('song/', views.song_search, name="song_search"),
+    path('song/', views.song_search, name="song"),
     path('song/<str:song_name>',views.song_detail, name="song"),
     path('recommend-songs/',views.recommend_songs, name="recommend_songs"),
     path('recommend-songs/<str:song_name>',views.recommend_songs, name="recommend_songs"),
